7674c393672032a09265f7a2ab9df98fa8fc827f
Artyom <art161994@yandex.ru>
2025-07-13 18:39:02 +0200

    fix: fix problems with deploy

--- class-components/my-app/src/App.tsx
+++ class-components/my-app/src/App.tsx
@@ -4,10 +4,10 @@ import Header from './components/Header';
 import Main from './components/Main';
 
 class App extends Component {
-  render() {
+  public render(): React.ReactNode {
     return (
       <ErrorBoundary>
-        <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+        <div className="app-container">
           <Header />
           <Main />
         </div>
