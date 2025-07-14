ef29ef6211e7ecce0d2c7d4db103f3ceba2d69e2
Artyom <art161994@yandex.ru>
2025-07-14 18:12:49 +0200

    fix: fix problem with search component

--- class-components/my-app/src/components/Header.tsx
+++ class-components/my-app/src/components/Header.tsx
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import Search from './Search';
 
 class Header extends Component {
   public render(): React.ReactNode {
     return (
       <header>
         <h1>Pokemon Search</h1>
-        <Search />
       </header>
     );
   }
