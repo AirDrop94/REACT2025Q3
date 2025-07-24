075be453c8c34c4de53e836f339076a0f514004c
Artyom <art161994@yandex.ru>
2025-07-24 12:25:54 +0200

    feat: finish master view realization

--- class-components/my-app/src/App.tsx
+++ class-components/my-app/src/App.tsx
@@ -4,11 +4,15 @@ import Layout from './layouts/Layout';
 import MainPage from './pages/MainPage';
 import AboutPage from './pages/AboutPage';
 import NotFoundPage from './pages/NotFoundPage';
+import PokemonDetail from './components/PokemonDetail';
 
 const App = () => (
   <Routes>
     <Route path="/" element={<Layout />}>
-      <Route index element={<MainPage />} />
+      <Route path="" element={<MainPage />}>
+        <Route index element={<div />} />
+        <Route path="" element={<PokemonDetail />} />
+      </Route>
       <Route path="about" element={<AboutPage />} />
       <Route path="*" element={<NotFoundPage />} />
     </Route>
--- class-components/my-app/src/pages/MainPage.tsx
+++ class-components/my-app/src/pages/MainPage.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import PokemonDetail from '../components/PokemonDetail';
 import CardList from '../components/CardList';
 import Search from '../components/Search';
-import { useSearchParams } from 'react-router-dom';
+import { useSearchParams, Outlet } from 'react-router-dom';
 import { usePokemonList } from '../hooks/usePokemonList';
 
 const MainPage = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const searchTerm = searchParams.get('q') || '';
   const page = Number(searchParams.get('page')) || 1;
-  const offset = (page - 1) * 20;
 
-  const { pokemons, loading, error } = usePokemonList(searchTerm, offset);
+  const { pokemons, loading, error } = usePokemonList(searchTerm, page);
 
   const handleSearch = (term: string) => {
     setSearchParams({ q: term, page: '1' });
@@ -21,14 +19,13 @@ const MainPage = () => {
     <div style={{ display: 'flex', width: '100%' }}>
       <main style={{ flex: 1, padding: '1rem' }}>
         <Search onSearch={handleSearch} />
-
         {loading && <p>Loading...</p>}
         {error && <p style={{ color: 'red' }}>{error}</p>}
         {!loading && !error && <CardList pokemons={pokemons} />}
       </main>
 
       <aside style={{ width: '300px', borderLeft: '1px solid #ccc' }}>
-        <PokemonDetail />
+        <Outlet />
       </aside>
     </div>
   );
