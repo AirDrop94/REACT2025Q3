1070443e4b8bb31a0f981d8a03252ff1bd02359a
Artyom <art161994@yandex.ru>
2025-07-25 09:54:49 +0200

    fix: fix problem with browsing

--- class-components/my-app/src/pages/AboutPage.tsx
+++ class-components/my-app/src/pages/AboutPage.tsx
@@ -4,7 +4,7 @@ const AboutPage: React.FC = () => {
   return (
     <div>
       <h2>About This App</h2>
-      <p>Author: Your Name</p>
+      <p>Author: Artsiom Luksha</p>
       <a href="https://rs.school/react/" target="_blank" rel="noopener noreferrer">
         RS School React Course
       </a>
