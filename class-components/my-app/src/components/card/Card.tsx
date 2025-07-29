6248ce3948d0b04bed4731b6dafbfad6ea9c801e
Artyom <art161994@yandex.ru>
2025-07-29 17:23:50 +0200

    feat: change Card.tsx

--- class-components/my-app/src/components/card/Card.tsx
+++ class-components/my-app/src/components/card/Card.tsx
@@ -10,32 +10,36 @@ interface Props {
 }
 
 const Card: React.FC<Props> = ({ pokemon, onClick }) => {
+  const dispatch = useAppDispatch();
+  const selectedItems = useAppSelector((state) => state.selectedItems.items);
+  const isSelected = selectedItems.some((item) => item.name === pokemon.name);
+
   const { name, url } = pokemon;
   const idMatch = url.match(/\/pokemon\/(\d+)\//);
   const id = idMatch ? idMatch[1] : 'unknown';
 
-  const dispatch = useAppDispatch();
-  const isSelected = useAppSelector((state) => state.selectedItems.items.some((item) => item.name === name));
+  const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
 
-  const handleCheckboxChange = () => {
-    dispatch(toggleItem({ ...pokemon, id }));
-  };
-
-  const handleCardClick = () => {
-    if (onClick && id !== 'unknown') {
+  const handleClick = () => {
+    if (onClick) {
       onClick(name);
     }
   };
 
+  const handleCheckboxChange = () => {
+    dispatch(toggleItem(pokemon));
+  };
+
   return (
-    <div className="card" onClick={handleCardClick}>
+    <div className={`card ${isSelected ? 'card--selected' : ''}`} onClick={handleClick}>
       <input
         type="checkbox"
         checked={isSelected}
         onChange={handleCheckboxChange}
         onClick={(e) => e.stopPropagation()}
+        className="card__checkbox"
       />
-      <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`} alt={name} />
+      <img src={imageUrl} alt={name} />
       <h3>{name}</h3>
     </div>
   );
